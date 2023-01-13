9d43d4911dc76e0ce88b9eb1e4e443db515eae97
KAOZ <kelvinalvescosta@hotmail.com>
2023-01-12 23:07:31 -0300

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+function toggleVideo(){
+    var trailer = document.querySelector('.trailer');
+    var video = document.querySelector('video');
+    trailer.classList.toggle('active');
+
+    video.currentTime = 0;
+    video.pause();
+}
